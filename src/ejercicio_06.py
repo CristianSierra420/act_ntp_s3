3efa1878dd5a56846fc2ca45df408f47529ca29c
cristian <sierraduquecris15@outlook.es>
2025-07-24 09:43:57 -0500

    e6

---
+++ src/ejercicio_06.py
@@ -0,0 +1,7 @@
+### Mediante un ciclo while, genera y muestra los primeros 15 múltiplos de 3, comenzando desde 3.
+
+i = 1
+while i <= 15:
+    multiplo = 3 * i
+    print(f"3 x {i} = {multiplo}")
+    i += 1
